ffda4ffd243d1e2a5b9d0a7248ad6d5981021b5c
iArney <iarnoldraphael@gmail.com>
2023-04-27 17:28:25 -0700

    initiated react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-     <Home />
-    </div>
+    <BrowserRouter>
+     <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/about" element={<About />} />
+      <Route exact path="/contact" element={<Contact />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
